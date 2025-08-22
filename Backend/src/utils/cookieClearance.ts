09e2fa4f751e1f363df388a071919fbe439158cb
Altamish Sakhawat <altamishsakhawat7412@gmail.com>
2025-08-22 21:32:44 +0500

    Update cookieClearance.ts

--- Backend/src/utils/cookieClearance.ts
+++ Backend/src/utils/cookieClearance.ts
@@ -1,17 +1,10 @@
 import { Response } from "express";
+import AuthCookieManager from "./authCookieManager";
 
 export const clearAuthCookie = (res: Response) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-  });
+  AuthCookieManager.clearAuthCookies(res);
 };
+
 export const addCookie = (res: Response, token: string) => {
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
-  });
+  AuthCookieManager.setAuthCookie(res, token);
 };
